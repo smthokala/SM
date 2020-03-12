0d2e80b0b2b720e7523f61ab5f8b6e1454a117e9
smthokala <sumanth.thokala@gmail.com>
2020-03-12 09:54:48 +0530

    commit2withVPC

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
 region = "${var.region}"
 }
+resource "aws_vpc" "Love_VPC" {
+  cidr_block       = "${var.VPC_Cidr}"
+  instance_tenancy = "dedicated"
+tags = {
+    Name = "Love_VPC"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
 variable "region" {
 default = "us-east-1"
 }
+variable  "VPC_Cidr" {
+default = "10.0.0.0/16"
+}
